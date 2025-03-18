91391f31a3b43bd479ecbcff24c22a56beb3c0ac
leonardorodriguezherediapucrsonline2 <leonardorodriguezherediapucrsonline2@gmail.com>
2025-03-17 21:56:31 -0300

    aula 06 - exercicios

--- aula05_recursao/AppRecursao.java
+++ aula05_recursao/AppRecursao.java
@@ -1,11 +1,36 @@
 package aula05_recursao;
 
+import utils.ArrayUtils;
+
+import java.util.ArrayList;
+
 public class AppRecursao {
     public static void main(String[] args) {
-        System.out.println(fatorialRecursivo(10));
-        System.out.println(fatorialNaoRecursivo(10));
-
+        int a[] = {88,75,15,99,63,28};
+        ArrayUtils.imprimirComPosicao(a);
+        inverterArrayRecursivo(a, 0);
+        System.out.println();
+        ArrayUtils.imprimirComPosicao(a);
     }
+    public static void inverterArrayRecursivo(int[] a, int aux) {
+        if(aux>=(a.length/2)) return;
+        int temp = a[aux];
+        a[aux] = a[a.length-1-aux];
+        a[a.length-1-aux] = temp;
+        inverterArrayRecursivo(a, aux + 1);
+    }
+    public static void inverterArrayNaoRecursivo(int[] a) {
+        for (int i = 0; i < (a.length/2); i++) {
+            int temp = a[i];
+            a[i]            = a[a.length-1-i];
+            a[a.length-1-i] = temp;
+        }
+    }
+
+
+
+
+
     public static int fatorialRecursivo(int n) {
         if(n<0) return -1;
         else if(n==0 || n==1) return 1;
@@ -24,6 +49,7 @@ public class AppRecursao {
     }
 
 
+
     public static int somatorio(int n) {
         if(n==0) return 0;
         else {
@@ -31,12 +57,7 @@ public class AppRecursao {
         }
     }
 
-    public static void inverterArrayNaoRecursivo(int[] a) {
 
-    }
-    public static void inverterArrayRecursivo(int[] a, int aux) {
-        ///
-    }
 
 
 
