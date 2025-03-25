d7d41dc55b66531e865d01d97aae7d8c2ae32b89
leonardorodriguezherediapucrsonline2 <leonardorodriguezherediapucrsonline2@gmail.com>
2025-03-24 21:24:24 -0300

    aula 06 - pesquisa array

--- aula06_pesquisaarrays/Exercicios.java
+++ aula06_pesquisaarrays/Exercicios.java
@@ -1,12 +1,14 @@
 package aula06_pesquisaarrays;
 
 public class Exercicios {
-    public int[] mergear(int[] a, int b[]) {
+    public static int[] mergear(int[] a, int b[]) {
         return null;
     }
 
     public static void main(String[] args) {
         int[] a = {14, 10, 20};
         int[] b = {15, 32, 12};
+
+        int[] m = mergear(a, b);
     }
 }
