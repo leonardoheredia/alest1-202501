db12f2d8c5453226a334184884b37ec5bfc92f3f
leonardorodriguezherediapucrsonline2 <leonardorodriguezherediapucrsonline2@gmail.com>
2025-03-24 22:02:00 -0300

    aula 07 - pesquisa array

--- aula06_pesquisaarrays/Exercicios.java
+++ aula06_pesquisaarrays/Exercicios.java
@@ -1,14 +1,43 @@
 package aula06_pesquisaarrays;
 
+import utils.ArrayUtils;
+
 public class Exercicios {
     public static int[] mergear(int[] a, int b[]) {
-        return null;
+        int[] m = new int[a.length+b.length];
+        for (int i = 0; i < a.length; i++) {
+            m[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            m[a.length+i] = b[i];
+        }
+        return m;
     }
+    public static int[] mergearOrdenado(int[] a, int[] b) {
+        int[] m = new int[a.length + b.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(k < m.length) {
+            ArrayUtils.imprimir(m);
+            if(a[i] < b[j]) {
+                m[k] = a[i];
+                i++;
+                k++;
+            }
+            else {
+                m[k] = b[j];
+                j++;
+                k++;
+            }
+        }
+        return m;
+    }
+        public static void main(String[] args) {
+        int[] a = {10, 14, 20};
+        int[] b = {12, 15, 32};
 
-    public static void main(String[] args) {
-        int[] a = {14, 10, 20};
-        int[] b = {15, 32, 12};
-
-        int[] m = mergear(a, b);
+        int[] m = mergearOrdenado(a, b);
+        ArrayUtils.imprimir(m);
     }
 }
